e376ca1546440d54cfe8c1c4472e3bd18f5419a4
Youbiao He <hybtalented@163.com>
2021-07-26 20:33:33 +0800

    完成光电传感器实验

--- 08_photo_interrupter/08_photo_interrupter.c
+++ 08_photo_interrupter/08_photo_interrupter.c
@@ -1,15 +1,15 @@
 /**************************************
- * 文件名 07_reed_switch.c
+ * 文件名 08_photo_interrupter.c
  * 版本 2.0
  * 作者 何友表
- * 说明 干簧管传感器实验，未检测到磁性物质时绿灯亮，检测到磁性物质时红灯亮
+ * 说明 光电传感器实验，没有物体遮挡时绿灯亮，物体遮挡时红灯亮
  *************************************/
 
 #include <stdio.h>
 #include <wiringPi.h>
 
 typedef unsigned char uchar;
-#define REED_SWITCH_SIG_PIN 0
+#define PHOTO_INTERRUPTER_SIG_PIN 0
 #define RED_PIN 1
 #define GREEN_PIN 2
 
@@ -18,22 +18,22 @@ int main() {
     printf("wiringPi Setup failed");
     return 1;
   }
-  pinMode(REED_SWITCH_SIG_PIN, INPUT);
+  pinMode(PHOTO_INTERRUPTER_SIG_PIN, INPUT);
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
   while (1) {
-    if (0 == digitalRead(REED_SWITCH_SIG_PIN)) {
+    if (0 == digitalRead(PHOTO_INTERRUPTER_SIG_PIN)) {
       delay(10);
-      if (0 == digitalRead(REED_SWITCH_SIG_PIN)) {
-        // 干簧管检测到磁性物质
+      if (0 == digitalRead(PHOTO_INTERRUPTER_SIG_PIN)) {
+        // 光电传感器无物体遮挡
         digitalWrite(RED_PIN, LOW);
         digitalWrite(GREEN_PIN, HIGH);
       }
 
-    } else if (1 == digitalRead(REED_SWITCH_SIG_PIN)) {
-      // 干簧管未检测到磁性物质
+    } else if (1 == digitalRead(PHOTO_INTERRUPTER_SIG_PIN)) {
       delay(10);
-      if (1 == digitalRead(REED_SWITCH_SIG_PIN)) {
+      if (1 == digitalRead(PHOTO_INTERRUPTER_SIG_PIN)) {
+        // 光电传感器有物体遮挡
         digitalWrite(RED_PIN, HIGH);
         digitalWrite(GREEN_PIN, LOW);
       }
